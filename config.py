843cfd48aff7fe0ca705a41cc447b87140db0c0f
Cryp70m4n <70487877+Cryp70m4n@users.noreply.github.com>
2021-11-26 23:30:14 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Client ID
+clinet_id = "client_id_here"
+
+#Banner check
+banner_check = True
+
+#Target check
+target_check = True
+
+#Live title check
+live_title_check = True
+
+#Category check
+category_check = True
+
+#Followers check
+followers_check = True
+
+#Current viewers check
+current_viewers_check = True
+
+#Subscribers check
+subscriber_check = True
+
+#start check
+start_check = True
+
+#Is live check
+is_live_check = True
