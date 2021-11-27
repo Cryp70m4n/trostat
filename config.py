06e59798f1d3dab6a0a4dce8bf2f7d56cce072bf
Cryp70m4n <70487877+Cryp70m4n@users.noreply.github.com>
2021-11-27 09:13:43 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#Client ID
-clinet_id = "client_id_here"
+import json
 
-#Banner check
-banner_check = True
 
-#Target check
-target_check = True
+class TrostatConfig():
+    def __init__(self):
+        #Client ID
+        self.clinet_id = "client_id_here"
 
-#Live title check
-live_title_check = True
+        #Banner check
+        self.banner_check = True
 
-#Category check
-category_check = True
+        #Target check
+        self.target_check = True
 
-#Followers check
-followers_check = True
+        #Live title check
+        self.live_title_check = True
 
-#Current viewers check
-current_viewers_check = True
+        #Category check
+        self.category_check = True
 
-#Subscribers check
-subscriber_check = True
+        #Followers check
+        self.followers_check = True
 
-#start check
-start_check = True
+        #Current viewers check
+        self.current_viewers_check = True
 
-#Is live check
-is_live_check = True
+        #Subscribers check
+        self.subscriber_check = True
+
+        #start check
+        self.start_check = True
+
+        #Is live check
+        self.is_live_check = True
+
+    @staticmethod
+    def ParseConfig(config_path):
+        f = open(config_path, "rb")
+        s = json.load(f)
+
+        td = vars(TrostatConfig())
+        td = td | s
+
+        t = TrostatConfig()
+        t.__dict__ = json.loads(json.dumps(td))
+
+        return t
