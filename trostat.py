53663c407cdcc9f3f4d9a7c9e5d214e9a6cc16a8
Cryp70m4n <70487877+Cryp70m4n@users.noreply.github.com>
2021-11-27 09:13:11 +0000

    Update trostat.py

--- trostat.py
+++ trostat.py
@@ -4,13 +4,14 @@ import requests
 import json
 import time
 
-#Import configs
+#Exporting configs
 from config import *
 
+c = TrostatConfig.ParseConfig("config.json")
 
 
 #Banner
-if banner_check == True:
+if c.banner_check == True:
     with open("banner") as f:
         banner = f.read()
         print(banner)
@@ -20,10 +21,9 @@ target= input("Enter target:")
 
 
 #response
-response = requests.post("https://open-api.trovo.live/openplatform/channels/id",headers={"Accept": "application/json","Client-ID": clinet_id},json={"username":target})
+response = requests.post("https://open-api.trovo.live/openplatform/channels/id",headers={"Accept": "application/json","Client-ID": c.client_id},json={"username":target})
 
 
-#Check if request was valid
 if 400 == response.status_code :
     print("Error proceeding request")
     quit()
@@ -34,41 +34,41 @@ data = json.loads(response.text)
 
 
 #target
-if target_check == True:
+if c.target_check == True:
     print("Target username:", data['username'])
 
 
 #title
-if live_title_check == True:
+if c.live_title_check == True:
     print("Live title:", data['live_title'])
 
 
 #category
-if category_check == True:
+if c.category_check == True:
     print("Category:", data['category_name'])
 
 
 #followers
-if followers_check == True:
+if c.followers_check == True:
     print("Followers:", data['followers'])
 
 
 #viewers
-if current_viewers_check == True:
+if c.current_viewers_check == True:
     print("Current viewers:", data['current_viewers'])
 
 #subscribers
-if subscriber_check == True:
+if c.subscriber_check == True:
     print("Subscribers:", data['subscriber_num'])
 
 #started live
-if start_check == True:
+if c.start_check == True:
     start = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(data['started_at'])))
     print("Started live at:", start)
 
 
 #is live
-if is_live_check == True:
+if c.is_live_check == True:
     if data['is_live'] == True:
         check = "Streaming"
 
